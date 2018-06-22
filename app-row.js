03dc2e6f7966e67561be59efef79f201c50ca3b9
Bob Davies <admin@bobbigmac.com>
2018-06-22 20:14:37 +0100

    Binding placeholder

--- app-row.js
+++ app-row.js
@@ -9,6 +9,7 @@ class AppRow extends HTMLElement {
 			hasState: true,
 			number: 0
 		}
+		//Render, and backup original children (not efficient, or responsive, gah)
 		this.childs = Array.prototype.map.call(this.childNodes, node => 
 			(node.render && node.render()) || 
 			node.textContent || 
@@ -20,11 +21,19 @@ class AppRow extends HTMLElement {
 	}
 	connectedCallback() {
 	    this.innerHTML = this.render();
-	    setInterval(() => ++this.state.number && (this.innerHTML = this.render()), 1000);
+	    setInterval(() => ++this.state.number && this.rerender(), 1000);
 	}
 	// disconnectedCallback() {}
 	attributeChangedCallback(attrName, oldVal, newVal) {
 		console.log('attributeChangedCallback', attrName, oldVal, newVal)
+	}
+	increment() {
+		console.log('incrementing', this.number);
+		this.number++;
+		this.rerender();
+	}
+	rerender() {
+		this.innerHTML = this.render();
 	}
 	render() {
 		const row = document.querySelector('#myRow');
@@ -32,10 +41,11 @@ class AppRow extends HTMLElement {
 		const state = this.state;
 		const props = this.props;
 		
-		// console.log('this.props', this.props);
 		return template.interpolate(Object.assign({ props }, { state }, {
-			// children: this.childNodes
-			children: this.childs
+			children: this.childs,
+			//TODO: This (of course) doesn't bind, but renders as text
+			//TODO: Interpolate needs to be more complex to bind functions
+			onClick: this.increment
 		}));
 	}
 }
